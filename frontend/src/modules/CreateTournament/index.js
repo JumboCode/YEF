c95f666ef5f53fef3d694150e818698810f76f07
Dan Katz <danielkatz125@gmail.com>
2018-09-25 18:07:35 -0400

    add barebones create tournament page

---
+++ frontend/src/modules/CreateTournament/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class CreateTournament extends Component {
+  render() {
+    return <div>Create Tournament</div>;
+  }
+}
+
+export default CreateTournament;
