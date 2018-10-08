5d8aace2e720c47998c116f76dafb6ce49d01675
Meguna Okawa <megunaokawa@gmail.com>
2018-10-08 15:14:55 -0400

    updated sign-in form with email and password validation.

--- frontend/src/modules/SignIn/index.js
+++ frontend/src/modules/SignIn/index.js
@@ -1,39 +1,70 @@
 import React, { Component } from "react";
 
-
 class SignIn extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state={
-			email:'',
-			password:''
+		this.state = {
+			email: '',
+			password: ''
+		};
+		this.props = {
+			title: 'Sign In Page'
 		}
+		this.setEmail = this.setEmail.bind(this);
+		this.setPassword = this.setPassword.bind(this);
 	}
 
+	setEmail(event) {
+		this.setState({email: event.target.value});
+	}
+
+	setPassword(event) {
+		this.setState({password: event.target.value});
+	}
+
+	validate(email,password) {
+		const emailRegex = RegExp(/.+\@.+\..+/);
+		return {
+			email: !emailRegex.test(email),
+			password: password.length === 0
+		};
+	}
+
+
 	render() {
+		const invalidInputClass = "error"; //class name assigned to input elements with invalid values
+		const errors = this.validate(this.state.email, this.state.password);
+
 		return (
 			<form>
 				<label>
 					Email:
-					<input type="text" email={this.state.email} onChange={this.setEmail} />
+					<input 
+						className={errors.email ? invalidInputClass : ""} 
+						type="text" 
+						value={this.state.email} 
+						placeholder="Enter email here" 
+						onChange={this.setEmail} 
+					/>
 				</label>
 				<br/>
 				<label>
 					Password:
-					<input type="password" email={this.state.password} onChange={this.setPassword} />
+					<input 
+						className={errors.password ? invalidInputClass : ""} 
+						type="password" 
+						value={this.state.password} 
+						placeholder="Enter password here" 
+						onChange={this.setPassword} 
+					/>
 				</label>
+				<button>Submit</button>
 			</form>
 
 		);
 	}
 
-	setEmail(event) {
-		this.setState({email: event.target.email});
-	}
 
-	setPassword(event) {
-		this.setState({password: event.target.password});
-	}
 }
 
 
