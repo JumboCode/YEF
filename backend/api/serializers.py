002c38643413a1b527e290981b5d523fa9f459e4
Manish Aryal <maryal01@Manishs-MacBook-Pro.local>
2018-10-09 20:42:39 -0400

    added tournament serializer without the foreign key

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from api.models import Team, Tournament
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,10 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+class TeamSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Team
+		fields = ('name', 'city', 'clubName', 'member1', 'member2', 'member3')
+
+# which field for the model you want to return
--- backend/src/urls.py
+++ backend/src/urls.py
@@ -23,8 +23,8 @@ from api import views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-router.register(r'tournaments', views.TournamentViewSet)
-
+#router.register(r'tournaments', views.TournamentViewSet)
+router.register(r'teams',views.TeamViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
