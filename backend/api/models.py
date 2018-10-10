2a32cc05a202a09227c69bded78dff1e9ed57a7b
Manish Aryal <maryal01@Manishs-MacBook-Pro.local>
2018-10-09 20:14:25 -0400

    added Team model

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,3 +7,12 @@ class Tournament(models.Model):
     location = models.CharField(max_length = 30)
     start_date = models.DateField()
     end_date = models.DateField()
+
+class Team(models.Model): #check the Model to see what the inherited class consist
+	name = models.CharField(max_length=255)
+	city = models.CharField(max_length=512)
+	clubName = models.CharField(max_length=512)
+	member1 = models.CharField(max_length=255)
+	member2 = models.CharField(max_length=255)
+	member3 = models.CharField(max_length=255)
+	tournamentID = models.ForeignKey(Tournament, on_delete=models.CASCADE) #check what models.CASCADE is doing
