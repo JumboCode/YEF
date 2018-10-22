6db8b8787c4fcf1a5eda5d58c5d5e3837141b80e
Nivi Nath <nivedita.nath@tufts.edu>
2018-10-21 22:00:15 -0400

    styling changes with flex

--- frontend/src/modules/AddTeam/index.js
+++ frontend/src/modules/AddTeam/index.js
@@ -4,12 +4,14 @@ import "./styles.css"
 class AddTeam extends Component {
     constructor(props) {
 	super(props);
-	this.state = {clubName: '', city: '', teamName: '', teamMembers: ''};
+	this.state = {clubName: '', city: '', teamName: '', teamMembers1: '', teamMembers2: '', teamMembers3: ''};
 
 	this.clubNameChange = this.clubNameChange.bind(this);
 	this.cityChange = this.cityChange.bind(this);
 	this.teamNameChange = this.teamNameChange.bind(this);
-	this.teamMembersChange = this.teamMembersChange.bind(this);
+	this.teamMembers1Change = this.teamMembers1Change.bind(this);
+	this.teamMembers2Change = this.teamMembers2Change.bind(this);
+	this.teamMembers3Change = this.teamMembers3Change.bind(this);
 	this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -25,8 +27,16 @@ class AddTeam extends Component {
 	this.setState({teamName: event.target.value});
     }
 
-    teamMembersChange(event) {
-	this.setState({location: event.target.value});
+    teamMembers1Change(event) {
+	this.setState({teamMembers1: event.target.value});
+    }
+
+    teamMembers2Change(event) {
+	this.setState({teamMembers2: event.target.value});
+    }
+
+    teamMembers3Change(event) {
+	this.setState({teamMembers3: event.target.value});
     }
 
     handleSubmit(event) {
@@ -41,26 +51,30 @@ class AddTeam extends Component {
 	return (<form onSubmit={this.handleSubmit}>
 	<div className = "flex-container">
 
-	<p className = "input-label"> Club Name: </p>
-	<input type="text" name="clubName" value={this.state.clubName} onChange={this.clubNameChange}/> 
-	</div>
+		
+	<p> Add a Team </p>
+		
+	<p className = "input-label"> Club Name: 
+	<input type="text" name="clubName" value={this.state.clubName} onChange={this.clubNameChange}/></p> 
 
-	<div className = "flex-container">
-	<p className= "input-label"> City: </p>
-	<input type="text" name="city" value={this.state.city} onChange={this.cityChange}/>
-	</div>
 
-	<div className = "flex-container">
-	<p className = "input-label"> Team Name: </p>
-	<input type="text" name="teamName" value={this.state.teamName} onChange={this.teamNameChange}/>
-	</div>
 
-	<div className = "flex-container">
+	<p className= "input-label"> City: 
+	<input type="text" name="city" value={this.state.city} onChange={this.cityChange}/></p>
+
+
+
+	<p className = "input-label"> Team Name: 
+	<input type="text" name="teamName" value={this.state.teamName} onChange={this.teamNameChange}/></p>
+
+
+
 	<p className = "input-label"> Team Members: </p>
-	<input type="text" name="teamMembers" value={this.state.teamMembers} onChange={this.teamMembersChange}/>
-	</div>
+	<input type="text" name="teamMembers1" value={this.state.teamMembers1} onChange={this.teamMembers1Change}/>
+	<input type="text" name="teamMembers2" value={this.state.teamMembers2} onChange={this.teamMembers2Change}/>
+	<input type="text" name="teamMembers3" value={this.state.teamMembers3} onChange={this.teamMembers3Change}/>
 
-	<div className = "flex-container">
+	<p></p>
 	<input type="submit" value="Submit"/> 
 	</div>
 
