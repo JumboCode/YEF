8f878dba95caff6e7a139096786bfb193887544a
Manish Aryal <maryal01@Manishs-MacBook-Pro.local>
2018-10-24 21:05:08 -0400

    added loading list tournaments

--- frontend/src/modules/TournamentsList/index.js
+++ frontend/src/modules/TournamentsList/index.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-class TournamentsList extends Component {
+class TournamentList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8000/tournaments/')
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            items: result,
+            isLoaded: true
+          });
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error: true
+          });
+        }
+      );
+  }
+
   render() {
-    return <div>Tournaments list</div>;
+    if (this.state.isLoaded && !this.state.error) {
+      console.log(this.state.items);
+      return (
+        <div>
+          <h3>List of Available Tournaments</h3>
+
+          <table>
+            <tbody>
+              {this.state.items.map(row => (
+                <tr>
+                  <td key={row.id}>{row.name}</td>
+                  <td key={row.id}>{row.location}</td>
+                  <td key={row.id}>{row.end_date}</td>
+                  <td key={row.id}>{row.start_date}</td>
+                </tr>
+              ))}
+              }
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return 'Loading...';
+    }
   }
 }
-
-export default TournamentsList;
+export default TournamentList;
