2b02da3ca99020d2aac7872bcecf16b966a35390
Andrew Gross <aleogross@gmail.com>
2018-10-30 20:24:30 -0400

    Added token authentication class to views and enabled users to make post requests

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,6 +7,8 @@ from rest_framework.authentication import TokenAuthentication
 from api.models import Team, Tournament
 from api.serializers import UserSerializer, GroupSerializer, TeamSerializer, TournamentSerializer
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -33,3 +35,17 @@ class TeamViewSet(viewsets.ModelViewSet):
 class TournamentViewSet(viewsets.ModelViewSet):
     queryset = Tournament.objects.all()
     serializer_class = TournamentSerializer
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
--- backend/src/urls.py
+++ backend/src/urls.py
@@ -30,7 +30,8 @@ router.register(r'tournaments', views.TournamentViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/',views.CustomAuthToken.as_view())
 ]
 
 #urlpatterns = [
