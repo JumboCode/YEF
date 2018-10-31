9ded2bad7950414d3611aedad148667f0be51d35
Andrew Gross <aleogross@gmail.com>
2018-10-30 20:30:03 -0400

    deleted unnecessary authentication classes

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -83,11 +83,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    )
+    ]
 }
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
