e3b9a4b4307a8dbcf2a98c6a4ee3b604fb9f9dd2
Carl Froneberger <carl.froneberger@tufts.edu>
2018-11-27 19:11:40 -0500

    basic tests. Getting used to how testing woroks

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from api.models import Tournament
+from api.models import Team
+import datetime
 # Create your tests here.
+
+class TournamentTestCase(TestCase):
+    def setUp(self):
+        time = datetime.date.today()
+        Tournament.objects.create(name = "first", location="here", start_date = time, end_date = time)
+        
+
+        def test_name(self):
+            first = Tournament.objects.get(name="first")
+            self.assertEqual(first.name)
